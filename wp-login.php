f96f7d95fbbe7fc7065a03892f3474bbc51f3a9a
onleftside <37473320+onleftside@users.noreply.github.com>
2018-03-17 12:09:57 +0100

    Add files via upload

---
+++ wp-login.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="cs-CZ">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="cs-CZ">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Login &lsaquo; Music T/\T &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='http://foto.itname.cz/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.9.2'></script>
+<script type='text/javascript' src='http://foto.itname.cz/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.js?ver=5.6.1'></script>
+<link rel='stylesheet' href='http://foto.itname.cz/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.2' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='http://foto.itname.cz/wp-content/plugins/jetpack/_inc/genericons/genericons/genericons.css?ver=3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='jetpack-sso-login-css'  href='http://foto.itname.cz/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.css?ver=5.6.1' type='text/css' media='all' />
+<div class="entry-content">
+				<div class="elementor elementor-320">
+			<div class="elementor-inner">
+				<div class="elementor-section-wrap">
+							<section data-id="e04d7e3" class="elementor-element elementor-element-e04d7e3 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-default">
+				<div class="elementor-row">
+				<div data-id="761b88c" class="elementor-element elementor-element-761b88c elementor-column elementor-col-100 elementor-top-column" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+					<div class="elementor-widget-wrap">
+				<div data-id="1467c30" class="elementor-element elementor-element-1467c30 elementor-button-info elementor-align-justify elementor-widget elementor-widget-button" data-element_type="button.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-button-wrapper">
+			<a href="http://foto.itname.cz/" class="elementor-button-link elementor-button elementor-size-xs">
+						<span class="elementor-button-content-wrapper">
+						<span class="elementor-button-text">Homepage</span>
+		</span>
+					</a>
+		</div>
+				</div>
+				</div>
+						</div>
+			</div>
+		</div>
+						</div>
+			</div>
+		</section>
+						</div>
+			</div>
+		</div>
+		<link rel='stylesheet' id='ssb-fontawesome-css'  href='http://foto.itname.cz/wp-content/plugins/sticky-side-buttons/assets/css/font-awesome.css?ver=4.8.5' type='text/css' media='all' />
+		<link rel='stylesheet' id='magnus-fonts-css'  href='//fonts.googleapis.com/css?family=Montserrat%3A400%2C700%7CKarla%3A400%2C700%2C400italic%2C700italic&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+		<link rel='stylesheet' id='magnus-style-css'  href='http://foto.itname.cz/wp-content/themes/magnus/style.css?ver=2.4' type='text/css' media='all' />
+		<link rel='stylesheet' id='elementor-icons-css'  href='http://foto.itname.cz/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=1.9.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='font-awesome-css'  href='http://foto.itname.cz/wp-content/plugins/elementor/assets/lib/font-awesome/css/font-awesome.min.css?ver=4.7.0' type='text/css' media='all' />
+		<link rel='stylesheet' id='elementor-animations-css'  href='http://foto.itname.cz/wp-content/plugins/elementor/assets/css/animations.min.css?ver=1.9.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='elementor-frontend-css'  href='http://foto.itname.cz/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=1.9.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='elementor-global-css'  href='http://foto.itname.cz/wp-content/uploads/elementor/css/global.css?ver=1516026511' type='text/css' media='all' />
+		<link rel='stylesheet' id='elementor-post-55-css'  href='http://foto.itname.cz/wp-content/uploads/elementor/css/post-55.css?ver=1516027272' type='text/css' media='all' />
+		<link rel='stylesheet' id='jetpack_css-css'  href='http://foto.itname.cz/wp-content/plugins/jetpack/css/jetpack.css?ver=5.6.1' type='text/css' media='all' />
+		<script type='text/javascript' src='http://foto.itname.cz/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+		<script type='text/javascript' src='http://foto.itname.cz/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+		<script type='text/javascript'>
+		/* <![CDATA[ */
+		var ssb_ui_data = {"z_index":"1"};
+		/* ]]> */
+		</script>
+		<style>
+				.jetpack-sso .message {
+					margin-top: 20px;
+				}
+
+				.jetpack-sso #login .message:first-child,
+				.jetpack-sso #login h1 + .message {
+					margin-top: 0;
+				}
+			</style>
+		<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://i0.wp.com/foto.itname.cz/wp-content/uploads/2017/06/cropped-cropped-002-6.jpg?fit=32%2C32" sizes="32x32" />
+<link rel="icon" href="https://i0.wp.com/foto.itname.cz/wp-content/uploads/2017/06/cropped-cropped-002-6.jpg?fit=192%2C192" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://i0.wp.com/foto.itname.cz/wp-content/uploads/2017/06/cropped-cropped-002-6.jpg?fit=180%2C180" />
+<meta name="msapplication-TileImage" content="https://i0.wp.com/foto.itname.cz/wp-content/uploads/2017/06/cropped-cropped-002-6.jpg?fit=270%2C270" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-cs-cz jetpack-sso jetpack-sso-form-display">
+		<div id="login">
+		<h1><a href="https://cs.wordpress.org/" title="Používáme WordPress" tabindex="-1">Music T/\T</a></h1>
+	<h2 id='clpt-logo'><img style='max-width: 100%;' src='http://foto.itname.cz/wp-content/uploads/2018/01/Logo_circle-1.jpg' alt='Music T/\T'/></h2>
+
+<form name="loginform" id="loginform" action="http://foto.itname.cz/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<div id="jetpack-sso-wrap">
+
+
+			<div id="jetpack-sso-wrap__action">
+				<a rel="nofollow" href="http://foto.itname.cz/wp-login.php?action=jetpack-sso" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Přihlásit se pomocí WordPress.com</span></a>
+									<p>
+											</p>
+							</div>
+
+							<div class="jetpack-sso-or">
+					<span>Or</span>
+				</div>
+
+				<a href="/wp-login.php?jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
+					Music T/\T 			</a>
+
+				<a href="http://foto.itname.cz/" class="jetpack-sso-toggle default">
+				® 2016-2018 ITNAME.CZ			</a>
+					</div>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Pamatovat si mě</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Přihlásit se" />
+		<input type="hidden" name="redirect_to" value="http://foto.itname.cz/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://foto.itname.cz/wp-login.php?action=lostpassword">Lost password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+</div>
+
+
+	    <style type="text/css">
+        html, html > body {
+                }
+
+        body.login div#login h1:first-child{
+                    display:none !important;
+                }
+        .login form{
+            margin-top: 0 !important;
+        }
+        #clpt-logo {
+            text-align: center;
+            padding: 20px;
+        }
+     </style>
+    <link rel='stylesheet' id='jetpack_css-css'  href='http://foto.itname.cz/wp-content/plugins/jetpack/css/jetpack.css?ver=5.6.1' type='text/css' media='all' />
+
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};/* ]]> */
+</script>
+<script type='text/javascript' src='http://foto.itname.cz/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=underscore,wp-util,wp-a11y,customize-base,customize-preview,customize-selective-refresh&amp;ver=4.9.2'></script>
+<!--[if lt IE 8]>
+<script type='text/javascript' src='http://foto.itname.cz/wp-includes/js/json2.min.js?ver=2015-05-03'></script>
+<![endif]-->
+	<div class="clear"></div>
+	</body>
+	</html>
